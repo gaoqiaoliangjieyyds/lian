fec32dea913d7a817a21be990e403fd872822c58
jia <1475697609@qq.com>
2024-06-30 15:10:56 +0800

    feature:jdk1.8后台远程调用中台服务

---
+++ duanlianjie/admin/src/main/java/com/jia/admin/controller/ShortLinkController.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jia.admin.controller;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.jia.admin.common.convention.result.Result;
+import com.jia.admin.remote.ShortLinkActualRemoteService;
+import com.jia.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.jia.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.jia.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.jia.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接后管控制层
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
+@RestController(value = "shortLinkControllerByAdmin")
+@RequiredArgsConstructor
+public class ShortLinkController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkActualRemoteService shortLinkRemoteService = new ShortLinkActualRemoteService() {
+    };
+
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+
+}
---
+++ duanlianjie/admin/src/main/java/com/jia/admin/remote/ShortLinkActualRemoteService.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jia.admin.remote;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.jia.admin.common.convention.result.Result;
+import com.jia.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.jia.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.jia.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.jia.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短链接中台远程调用服务
+ */
+public interface ShortLinkActualRemoteService {
+    /**
+     * 创建短链接
+     *
+     * @param requestParam 创建短链接请求参数
+     * @return 短链接创建响应
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8002/api/short-link/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 分页查询短链接
+     *
+     * @param requestParam 分页短链接请求参数
+     * @return 查询短链接响应
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8002/api/short-link/v1/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+
+}
